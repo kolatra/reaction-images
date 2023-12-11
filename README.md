48fc24b8565e891e44d73143b5dbbb4f36c7eda8
tyler <3821892+kolatra@users.noreply.github.com>
2023-12-11 16:38:26 -0700

    add cli
    closes #1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Gloomander Images
+
+A small tool to save/download Discord links saved in Gloomander.
+
+## usage
+```bash
+cargo run -- -f sqlite.db -o images/ 
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #![allow(unused)]
 use std::io::Write;
 
+use clap::Parser;
 use sqlx::{SqliteConnection, Connection, Row};
 use futures::TryStreamExt;
 
+#[derive(Debug, Parser)]
+#[clap(name = "basic")]
+struct Args {
+    #[arg(short, long, help = "The sqlite database file to read from")]
+    file: String,
+    #[arg(short, long, help = "The folder to write the images to")]
+    output_file: String,
+    #[arg(short, long, default_value = "urls.txt", help = "The file to write the urls to")]
+    urls_file: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut conn = SqliteConnection::connect(&std::env::var("DATABASE_URL")?).await?;
+    let args = Args::parse();
+    let db_url = String::from("sqlite://") + &args.file;
+    let mut conn = SqliteConnection::connect(&db_url).await?;
     let mut results_stream = sqlx::query("SELECT * FROM response_images").fetch(&mut conn);
     let mut i = 0;
 
@@ -25,13 +39,13 @@ async fn main() -> anyhow::Result<()> {
         let extension = s.split('.').last().unwrap();
         let bytes = reqwest::get(&s).await?.bytes().await?;
 
-        let mut file = std::fs::File::create(format!("images/{}.{}", i, extension))?;
+        let mut file = std::fs::File::create(format!("{}/{}.{}", args.output_file, i, extension))?;
         println!("writing to file");
         file.write_all(&bytes)?;
         i += 1;
     }
 
-    let mut file = std::fs::File::create("urls.txt")?;
+    let mut file = std::fs::File::create(args.urls_file)?;
     for url in urls {
         println!("writing url to disk");
         file.write_all(url.as_bytes())?;
